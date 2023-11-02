d03ca4f750ea1b10e6ad7d47d2f14d9e40686c6f
josielee1212 <josie.digitalad@gmail.com>
2023-11-02 14:33:36 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
 app.get('/', (req, res) => {
-  res.send('express app for movies')
+  res.redirect('/restaurants')
+})
+
+app.get('/restaurants', (req, res) => {
+  res.send('listing restaurants')
+})
+
+app.get('/restaurant/:id', (req, res) => {
+  const id = req.params.id
+  res.send(`read restaurant: ${id}`)
+})
+
+app.get('/restaurant/:id/cart', (req, res) => {
+  const id = req.params.id
+  res.send(`open cart in restaurant: ${id}`)
 })
 
 app.listen(port, () => {
